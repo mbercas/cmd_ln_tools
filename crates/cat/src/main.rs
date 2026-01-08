da29456053bfa15927d2923f1cb6d052f9a35a82
manuel <manuel@ezono.com>
2026-01-08 18:42:10 +0100

    Beautified file

--- crates/cat/src/main.rs
+++ crates/cat/src/main.rs
@@ -1,14 +1,13 @@
-use std::env;
-use std::fs;
-use std::error::Error;
 use clap::{command, Arg, ArgAction};
+use std::env;
+use std::error::Error;
+use std::fs;
 use std::io::{self, Write};
 
 /// Takes a vector of strings, where each string may have one or
 /// more EOL characters and separaes the lines to return a vector
 /// of single line strigns.
-fn unwrap_lines(data:Vec<String>) -> Vec<String> {
-    
+fn unwrap_lines(data: Vec<String>) -> Vec<String> {
     let mut output_data = vec![];
 
     for lines in data.iter() {
@@ -21,8 +20,7 @@ fn unwrap_lines(data:Vec<String>) -> Vec<String> {
 /// Appends a line number at the beggining of every line,
 /// if the ignore_blanks flag is set, does not add a number to
 /// empty lines
-fn append_line_number(data:Vec<String>, ignore_blanks:bool) -> Vec<String> {
-    
+fn append_line_number(data: Vec<String>, ignore_blanks: bool) -> Vec<String> {
     // Calculate the right alignment of the number column
     let nc = data.len().to_string().len();
 
@@ -37,13 +35,13 @@ fn append_line_number(data:Vec<String>, ignore_blanks:bool) -> Vec<String> {
                 s = String::new();
             }
             s
-            })
+        })
         .collect()
 }
 
-fn remove_consecutive_empty_lines(data:Vec<String>) -> Vec<String> {
+fn remove_consecutive_empty_lines(data: Vec<String>) -> Vec<String> {
     let mut empty_line_counter = 0;
-    
+
     data.into_iter()
         .filter(|line| {
             if line.is_empty() {
@@ -52,9 +50,8 @@ fn remove_consecutive_empty_lines(data:Vec<String>) -> Vec<String> {
                 empty_line_counter = 0;
             }
             empty_line_counter < 2
-        }
-        ).collect()
-    
+        })
+        .collect()
 }
 
 fn print_output(data: Vec<String>) {
@@ -67,27 +64,31 @@ fn print_output(data: Vec<String>) {
     }
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
-
     let matches = command!()
         .about("Concatenate FILE(s) to standard output")
         .arg(Arg::new("FILE").action(ArgAction::Append))
-        .arg(Arg::new("numbers")
-            .short('n')
-            .long("numbers")
-            .action(ArgAction::SetTrue)
-            .help("Prepends line numbers to the output"))
-        .arg(Arg::new("squeeze-blank")
-            .short('s')
-            .long("squeeze-blank")
-            .action(ArgAction::SetTrue)
-            .help("Remove consecutive empty lines"))
-        .arg(Arg::new("number-noblank")
-            .short('b')
-            .long("number-noblank")
-            .action(ArgAction::SetTrue)
-            .help("number nonempty output lines, overrides -n"))
+        .arg(
+            Arg::new("numbers")
+                .short('n')
+                .long("numbers")
+                .action(ArgAction::SetTrue)
+                .help("Prepends line numbers to the output"),
+        )
+        .arg(
+            Arg::new("squeeze-blank")
+                .short('s')
+                .long("squeeze-blank")
+                .action(ArgAction::SetTrue)
+                .help("Remove consecutive empty lines"),
+        )
+        .arg(
+            Arg::new("number-noblank")
+                .short('b')
+                .long("number-noblank")
+                .action(ArgAction::SetTrue)
+                .help("number nonempty output lines, overrides -n"),
+        )
         .get_matches();
 
     let input_files = matches
@@ -97,33 +98,31 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Vec<_>>();
 
     // Iterate over the valid input files and load the contents into memory
-    let mut contents : Vec<String> = vec![];
+    let mut contents: Vec<String> = vec![];
     let mut errors = vec![];
 
     for fname in input_files.iter() {
-
         match fs::read_to_string(fname) {
             Ok(data) => contents.push(data),
             Err(e) => {
                 eprintln!("Error reading file {fname}: {e}");
                 errors.push((fname, e));
-            },
+            }
         }
     }
 
     // Split lines with mulitple end of line into separate vector entries
     contents = unwrap_lines(contents);
-    
+
     if matches.get_flag("squeeze-blank") {
         contents = remove_consecutive_empty_lines(contents);
     }
     if matches.get_flag("number-noblank") {
-        contents =  append_line_number(contents, true);
+        contents = append_line_number(contents, true);
+    } else if matches.get_flag("numbers") {
+        contents = append_line_number(contents, false);
     }
-    else if matches.get_flag("numbers") {
-        contents =  append_line_number(contents, false);
-    }
-    
+
     print_output(contents);
 
     if errors.is_empty() {
@@ -133,8 +132,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         Err("Errors reported to stderr".into())
     }
 }
-
-
 
 #[cfg(test)]
 mod cat_tests {
@@ -147,74 +144,72 @@ mod cat_tests {
                 for i in 1..=n_lines {
                     lines.push(format!("Line {}", i).to_owned());
                 }
-                },
-            2 => {        
-                for i in 1..=n_lines {       
-                    lines.push(format!("Line {}\nLine {}", x_factor*i-1, x_factor*i).to_owned());
+            }
+            2 => {
+                for i in 1..=n_lines {
+                    lines.push(
+                        format!("Line {}\nLine {}", x_factor * i - 1, x_factor * i).to_owned(),
+                    );
                 }
-                },
+            }
             _ => panic!("Use a valid x_factor"),
         }
-            
-        
+
         lines
     }
 
-
     #[test]
     fn unwrap_lines_no_unwrapping() {
-        const N : usize = 100;
+        const N: usize = 100;
         let xfactor = 1;
         let orig_lines = generate_test_vector(N, xfactor);
-     
-        assert_eq!(N*xfactor, orig_lines.len());
+
+        assert_eq!(N * xfactor, orig_lines.len());
         let unwrapped = unwrap_lines(orig_lines);
-        assert_eq!(N*xfactor, unwrapped.len());
+        assert_eq!(N * xfactor, unwrapped.len());
 
         // running mulitple times should not make a difference
         let unwrapped = unwrap_lines(unwrapped);
-        assert_eq!(N*xfactor, unwrapped.len());
+        assert_eq!(N * xfactor, unwrapped.len());
 
         // Test the empty vector
         let unwrapped = unwrap_lines(vec![]);
         assert_eq!(0, unwrapped.len());
     }
 
-
     #[test]
     fn unwrap_lines_with_unwrapping() {
-        const N : usize = 100;
+        const N: usize = 100;
         let xfactor = 2;
         let orig_lines = generate_test_vector(N, xfactor);
-     
+
         assert_eq!(N, orig_lines.len());
         let unwrapped = unwrap_lines(orig_lines);
-        assert_eq!(N*xfactor, unwrapped.len());
-}
-
+        assert_eq!(N * xfactor, unwrapped.len());
+    }
 
     #[test]
     fn append_line_number_check_returned_size() {
         let mut orig_lines = vec![];
-        const N : usize = 100;
+        const N: usize = 100;
         let xfactor = 1;
         orig_lines = generate_test_vector(N, xfactor);
 
         let mod_lines = append_line_number(orig_lines, false);
-        assert_eq!(N*xfactor, mod_lines.len());
-   }
-    
+        assert_eq!(N * xfactor, mod_lines.len());
+    }
+
     #[test]
     fn append_line_number_check_indexing() {
         let mut orig_lines = vec![];
-        const N : usize = 100;
+        const N: usize = 100;
         let xfactor = 1;
         orig_lines = generate_test_vector(N, xfactor);
 
         let mod_lines = append_line_number(orig_lines, false);
         for (i, line) in mod_lines.iter().enumerate() {
             let mut tokens = line.split_whitespace();
-            assert_eq!(i+1, tokens.next().unwrap().parse::<usize>().unwrap())
+            assert_eq!(i + 1, tokens.next().unwrap().parse::<usize>().unwrap())
         }
     }
 
@@ -231,12 +226,11 @@ mod cat_tests {
 
         let mod_lines = remove_consecutive_empty_lines(orig_lines);
         assert_eq!(1, mod_lines.len()); // one line of the repeated chunk remains
-        
     }
-    
+
     #[test]
     fn remove_consecutive_empty_lines_check_count() {
-        const N : usize = 100;
+        const N: usize = 100;
         let xfactor = 1;
         let mut orig_lines = generate_test_vector(N, xfactor);
 
@@ -244,13 +238,13 @@ mod cat_tests {
             orig_lines.push(String::from(""));
         }
 
-        assert_eq!(N*xfactor+100, orig_lines.len());
+        assert_eq!(N * xfactor + 100, orig_lines.len());
         let mod_lines = remove_consecutive_empty_lines(orig_lines);
-        assert_eq!(N*xfactor+1, mod_lines.len());
+        assert_eq!(N * xfactor + 1, mod_lines.len());
 
         // calling the function again should not remove more lines
-        
+
         let mod_lines = remove_consecutive_empty_lines(mod_lines);
-        assert_eq!(N*xfactor+1, mod_lines.len());
+        assert_eq!(N * xfactor + 1, mod_lines.len());
     }
 }
